758ce59326f26f7c4230ddce82fc5a81a7119513
Andrew Brudnak <brudnak@icloud.com>
2019-04-17 00:17:28 -0700

    Lesson 7 from C# class at Rio Salado fall 2018

---
+++ lesson-007/A1Construction_Andrew_Brudnak_L7/A1Construction/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using A1Construction.Models;
+// Andrew Brudnak
+namespace A1Construction.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new CustomerModels();
+            return View(model);
+        }
+        
+        [HttpPost]
+        public ActionResult Index(CustomerModels model)
+        {
+            if (ModelState.IsValid) return RedirectToAction("ThankYou", model);
+
+            return View(model);
+        }
+
+        public ActionResult ThankYou(CustomerModels model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ lesson-007/A1Construction_Andrew_Brudnak_L7/A1Construction/Models/CustomerModels.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+// Andrew Brudnak
+namespace A1Construction.Models
+{
+    public class CustomerModels
+    {
+        [DisplayName("First name")] [Required] public string FirstName { get; set; }
+
+        [DisplayName("Last name")] [Required] public string LastName { get; set; }
+
+        [DisplayName("Company name")] public string CompanyName { get; set; }
+
+        [DisplayName("Project Cost")]
+        // Data type Currency alone will only allow a number to be entered
+        // But not if the user has JavaScript disabled in their browser. 
+        // Regex added with if on customer controller to handle in case JavaScript is disabled.
+        [DataType(DataType.Currency)]
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Project cost must be numeric up to two decimal points.")]
+        public decimal ProjectCost { get; set; }
+    }
+}
